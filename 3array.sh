2839af6c616b4704a824816bc672a40c40f409c1
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-07 22:50:37 +0530

    Array

--- 3array.sh
+++ 3array.sh
@@ -2,8 +2,8 @@
 
 Fruits=("Apple","Banana","Lemon")
 
-echo "First fruit is $Fruits[0]"
-echo "Second fruit is $Fruits[0]"
-echo "Third fruit is $Fruits[0]"
+echo "First fruit is ${Fruits[0]}"
+echo "Second fruit is ${Fruits[1]}"
+echo "Third fruit is ${Fruits[1]}"
 
 echo "Fruits are $Fruits[@]"
