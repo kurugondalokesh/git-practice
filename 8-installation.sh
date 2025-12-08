6a5f6ccbffff4f7d6992f7a8e7fb85de9c07e5a7
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-08 22:45:39 +0530

    installation

--- 8-installation.sh
+++ 8-installation.sh
@@ -8,3 +8,19 @@ then
     exit 1
 fi
 
+dnf list installed git
+
+if [ $? -ne 0 ]
+then 
+    echo "Git is not installed, will install it"
+    dnf install git -y
+    if [ $? -ne 0 ]
+    then
+        echo "Git not installed successfully"
+        exit 1
+    else    
+        echo "Git installed succesfully"
+    fi
+else 
+    echo "Git already installed"
+fi
