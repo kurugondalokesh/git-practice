796d99bfe20b2463551657caeb9e72efe8851e5b
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-08 21:49:21 +0530

    Datatype

---
+++ 6-spl-vars.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "All the arguments or variables passed to the script : $@"
+echo "Number of arguments that are passed : $#"
+echo "Script name : $0"
+echo "Current working directory : $pwd"
