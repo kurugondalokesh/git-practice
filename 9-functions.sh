49d9d15a416a146b80b5ec6e5439556857fdeb8f
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-09 22:27:11 +0530

    Functions

---
+++ 9-functions.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+VALIDATE (){
+    echo "Exit status is : $1"
+}
+
+if [ $USERID -ne 0 ]
+then
+    echo "Please run this script with root privelages"
+    exit 1
+fi
+
+dnf list installed git
+
+VALIDATE $?
+
+# if [ $? -ne 0 ]
+# then 
+#     echo "Git is not installed, will install it"
+#     dnf install git -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Git not installed successfully"
+#         exit 1
+#     else    
+#         echo "Git installed succesfully"
+#     fi
+# else 
+#     echo "Git already installed"
+# fi
+
+# dnf list installed mysql
+
+# if [ $? -ne 0 ]
+# then
+#     echo "Mysql is not installed, we are installiung it now"
+#     dnf install mysql -y
+#     if [ $? -ne 0 ]
+#     then
+#         echo "Mysql not installed successfully"
+#         exit 1
+#     else 
+#         echo "My sql installed succesfully"
+#     fi
+# else 
+#     echo "My sql is already installed"
+# fi
