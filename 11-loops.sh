3c3ac40e425efdac319898eb96f36feae193bcba
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-14 22:27:03 +0530

    Loops

---
+++ 11-loops.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+CHECKROOT (){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R Please run this script with root privelages $N"
+        exit 1
+    fi
+}
+
+VALIDATE (){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$2 is $R failed $N"
+        exit 1
+    else 
+        echo "$2 is $G successfull $N"
+    fi
+}
+
+
+CHECKROOT
+
+for package in $@
+do  
+    echo "$package"
+done
