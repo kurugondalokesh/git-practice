2f68e63da95752dad2b267858879cedbcc843854
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-14 22:11:14 +0530

    Loops

--- 10-loops.sh
+++ 10-loops.sh
@@ -1,10 +1,15 @@
-echo "1"
-echo "2"
-echo "3"
-echo "4"
-echo "5"
+# echo "1"
+# echo "2"
+# echo "3"
+# echo "4"
+# echo "5"
 
 for i in 1 2 3 4 
+do 
+    echo "$i"
+done
+
+for i in {1..10}
 do 
     echo "$i"
 done
