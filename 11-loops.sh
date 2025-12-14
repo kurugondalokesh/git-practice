d6408baad615f708da3b931dfbb7f441824d3955
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-14 22:38:25 +0530

    Loops

--- 11-loops.sh
+++ 11-loops.sh
@@ -25,7 +25,7 @@ CHECKROOT
 for package in $@
 do  
     dnf list installed $package
-    if [$? -ne 0]
+    if [ $? -ne 0 ]
     then
         echo "$package is not installed going to install it"
         dnf install $package -y
