c61388e45d99182858869b973f450bd95543d963
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2025-12-21 22:56:25 +0530

    set command

--- 13-set.sh
+++ 13-set.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# set -e   #setting automatic exit if any error appears
+set -e   #setting automatic exit if any error appears
 
 echo "Command before error"
 echooo "Command with error"
