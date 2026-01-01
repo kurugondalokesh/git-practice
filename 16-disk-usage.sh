21f4be625e3592cdd368116ed9c0f30d7889df71
kurugondalokesh <lokeshkurugonda2002@gmail.com>
2026-01-01 21:48:55 +0530

    Disk usage

--- 16-disk-usage.sh
+++ 16-disk-usage.sh
@@ -5,12 +5,10 @@ DISK_THRESHOLD=5
 
 while IFS= read -r line
 do
-    echo $line
     USAGE=$(echo $line | grep xfs | awk -F " " '{print $6f}' | cut -d "%" -f1)
     PARTITION=$(echo $line | grep xfs | awk -F " " '{print $NF}')
     if [ $USAGE -ge $DISK_THRESHOLD]
     then
         echo "$PARTITION is more than $DISK_THRESHOLD, current usage : $USAGE "
     fi
-
 done <<< $DISK_USAGE 
